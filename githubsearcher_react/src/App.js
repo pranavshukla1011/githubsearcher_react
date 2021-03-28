be0c4ab754a248757af72ebc216e54bfe27a2aa6
pranavshukla1011 <shuklapranav1011@gmail.com>
2021-03-29 05:11:55 +0530

    page 1 completed ____ now setting up react router

--- githubsearcher_react/src/App.js
+++ githubsearcher_react/src/App.js
@@ -4,6 +4,7 @@ import React, { Component } from 'react';
 import Navbar from './components/layouts/Navbar';
 import Users from './components/users/Users';
 import Search from './components/users/Search';
+import Alert from './components/layouts/alert'
 import axios from 'axios';
 import './App.css';
 
@@ -11,40 +12,60 @@ class App extends Component {
   state = {
     users: [],
     loading: false,
+    alert: null
   };
 
-  async componentDidMount() {
-    /* in react we cannot directly change value of state variable by using this.state.loading = true,
-    instead we do... */
+  // async componentDidMount() {
+  //   /* in react we cannot directly change value of state variable by using this.state.loading = true,
+  //   instead we do... */
 
-    //setState
-    this.setState({ loading: true }); //till data is not fetched
+  //   //setState
+  //   this.setState({ loading: true }); //till data is not fetched
 
-    const res = await axios.get(
-      `https://api.github.com/users?client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
-    );
+  //   const res = await axios.get(
+  //     `https://api.github.com/users?client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
+  //   );
 
-    this.setState({ users: res.data, loading: false });
-  }
+  //   this.setState({ users: res.data, loading: false });
+  // }
 
   searchUsers = async (text) => {
-    
+    this.setState({ loading: true });
+
     const res = await axios.get(
-      `https://api.github.com/search/users?q=client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
+      `https://api.github.com/search/users?q=${text}&client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
     );
 
     this.setState({ users: res.data.items, loading: false });
   };
 
+  clearUsers = () => {
+    this.setState({ users: [], loading: false });
+  };
+
+  setAlert = (msg,type) => {
+    this.setState({alert: {
+      msg:msg,
+      type:type
+    }})
+    setTimeout(() => this.setState({alert: null}),3000)
+  };
+
   render() {
-    const { loading, users } = this.state;
+    const { loading, users, alert } = this.state;
     return (
       <div className='App'>
         <Navbar title='Locate Github' icon='fab fa-github'></Navbar>
 
         <div className='container'>
           <br />
-          <Search searchUsers={this.searchUsers} />
+          <Alert alert={alert}/>
+          <Search
+            searchUsers={this.searchUsers}
+            clearUsers={this.clearUsers}
+            showClear={this.state.users.length > 0 ? true : false}
+            setAlert={this.setAlert}
+          />
           <br />
           <Users loading={loading} users={users}></Users>
         </div>
--- githubsearcher_react/src/components/users/Search.js
+++ githubsearcher_react/src/components/users/Search.js
@@ -1,40 +1,58 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 
 class Search extends Component {
   state = {
-    text: ''
+    text: '',
+  };
+
+  static propTypes = {
+    searchUsers: PropTypes.func.isRequired,
+    clearUsers: PropTypes.func.isRequired,
+    showClear: PropTypes.bool.isRequired,
+    setAlert: PropTypes.func.isRequired,
   };
 
   onChange = (e) => {
     this.setState({
-        [e.target.name]:e.target.value
-    })
-  }
-    
-  onSubmit = (e) =>{
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  onSubmit = (e) => {
     e.preventDefault();
-    this.props.searchUsers(this.state.text);
-    this.setState({text:''})
-  }
+    if (this.state.text === '') {
+      this.props.setAlert('Please Enter Something', 'light');
+    } else {
+      this.props.searchUsers(this.state.text);
+      this.setState({ text: '' });
+    }
+  };
 
   render() {
+    const { showClear, clearUsers } = this.props;
     return (
       <div>
-        <form onSubmit = {this.onSubmit} action='' className='form'>
+        <form onSubmit={this.onSubmit} action='' className='form'>
           <input
             type='text'
             name='text'
             placeholder='Search users...'
             value={this.state.text}
-            onChange = {this.onChange}
+            onChange={this.onChange}
           />
           <input
-            name = 'submit'
+            name='submit'
             type='submit'
             value='search'
             className='btn btn-dark btn-block'
           />
         </form>
+        {showClear === true && (
+          <button className='btn btn-light btn-block' onClick={clearUsers}>
+            Clear
+          </button>
+        )}
       </div>
     );
   }
